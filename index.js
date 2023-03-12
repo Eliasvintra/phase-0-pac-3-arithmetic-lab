3f21eca72abd0cb1acf068386399c52f03cf544a
Eliasvintra <rbnvintra.ke@gmail.com>
2023-03-12 15:21:47 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,46 @@
+function add() {
+
+}
+function subtract() {
+
+}
+function multiply () {
+
+}
+function divide (){
+
+}
+
+let a = 200;
+let b = 50;
+function add(a,b) {
+    return a + b;
+}
+
+function subtract(a,b) {
+    return a - b;
+}
+function multiply(a,b) {
+    return a * b;
+}
+function divide(a,b) {
+    return a / b;
+}
+
+let n = 10;
+function increment(n) {
+    ++n;
+    return n;
+}
+function decrement(n) {
+    return --n;
+}
+
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+
+let y = 4.2045;
+function preserveDecimal(y) {
+    return parseFloat(y);
+}
